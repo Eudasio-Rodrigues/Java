8574bbd3f1449138787320983f6e50727ded2056
Eudasio Rodrigues <79660796+Eudasio-Rodrigues@users.noreply.github.com>
2022-04-27 15:02:21 -0300

    Update Questao3.java

--- questao3/Questao3.java
+++ questao3/Questao3.java
@@ -1,6 +1,14 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ *Crie uma classe Elevador para armazenar as informações de um elevador de prédio.
+ *A classe deve armazenar o andar atual (térreo = 0), total de andares no prédio (desconsiderando o térreo),
+ *capacidade do elevador e quantas pessoas estão presentes nele. 
+ *A classe deve também disponibilizar os seguintes métodos:
+    Inicializar: que deve receber como parâmetros a capacidade do elevador e o total de andares no prédio (os elevadores sempre começam no térreo e vazio);
+    Entrar: para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço);
+    Sair: para remover uma pessoa do elevador (só deve remover se houver alguém dentro dele);
+    Subir: para subir um andar (não deve subir se já estiver no último andar);
+    Descer: para descer um andar (não deve descer se já estiver no térreo);
+    Obs.: Encapsular todos os atributos da classe (criar os métodos set e get).
  */
 
 package com.mycompany.questao3;

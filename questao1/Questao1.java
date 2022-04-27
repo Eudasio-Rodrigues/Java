b534f0d92f79a711b0a5f9affa9f63c3cbd85630
Eudasio Rodrigues <79660796+Eudasio-Rodrigues@users.noreply.github.com>
2022-04-27 14:56:57 -0300

    Update Questao1.java

--- questao1/Questao1.java
+++ questao1/Questao1.java
@@ -1,6 +1,8 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ *Crie uma classe para representar um jogador de futebol, com os atributos nome, posição, data de nascimento, nacionalidade, altura e peso.
+ *Crie os métodos públicos necessários para sets e gets e também um método para imprimir todos os dados do jogador. 
+ *Crie um método para calcular a idade do jogador e outro método para mostrar quanto tempo falta para o jogador se aposentar.
+ *Para isso, considere que os jogadores da posição de defesa se aposentam em média aos 40 anos, os jogadores de meio-campo aos 38 e os atacantes aos 35.
  */
 
 package com.mycompany.questao1;
